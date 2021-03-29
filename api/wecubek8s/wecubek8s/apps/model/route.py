3441c8b47cb9ea048c27d1bde335c0f301f69d52
roywu <roywu@webank.com>
2021-03-29 09:33:06 +0800

    remove get query route

--- api/wecubek8s/wecubek8s/apps/model/route.py
+++ api/wecubek8s/wecubek8s/apps/model/route.py
@@ -20,12 +20,12 @@ def add_routes(api):
     delete:
     '''
     api.add_route('/kubernetes/entities/cluster/query', controller.PostQueryCluster())
-    api.add_route('/kubernetes/entities/cluster', controller.GetQueryCluster())
+    # api.add_route('/kubernetes/entities/cluster', controller.GetQueryCluster())
     api.add_route('/kubernetes/entities/node/query', controller.PostQueryNode())
-    api.add_route('/kubernetes/entities/node', controller.GetQueryNode())
+    # api.add_route('/kubernetes/entities/node', controller.GetQueryNode())
     api.add_route('/kubernetes/entities/deployment/query', controller.PostQueryDeployment())
-    api.add_route('/kubernetes/entities/deployment', controller.GetQueryDeployment())
+    # api.add_route('/kubernetes/entities/deployment', controller.GetQueryDeployment())
     api.add_route('/kubernetes/entities/service/query', controller.PostQueryService())
-    api.add_route('/kubernetes/entities/service', controller.GetQueryService())
+    # api.add_route('/kubernetes/entities/service', controller.GetQueryService())
     api.add_route('/kubernetes/entities/pod/query', controller.PostQueryPod())
-    api.add_route('/kubernetes/entities/pod', controller.GetQueryPod())
+    # api.add_route('/kubernetes/entities/pod', controller.GetQueryPod())
