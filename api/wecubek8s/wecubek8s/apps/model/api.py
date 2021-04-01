0efe1a25b0e84ac897bbdcc06c3e221b9948aa76
roywu <roywu@webank.com>
2021-04-01 11:04:44 +0800

    update model api

--- api/wecubek8s/wecubek8s/apps/model/api.py
+++ api/wecubek8s/wecubek8s/apps/model/api.py
@@ -3,7 +3,9 @@
 from __future__ import absolute_import
 
 import logging
+import datetime
 
+from kubernetes import watch
 from talos.common import cache
 from talos.core import config
 from talos.core.i18n import _
@@ -29,6 +31,11 @@ class BaseEntity:
             results = [ret for ret in results if jsonfilter.match_all(filters, ret)]
         return results
 
+    def clear_cache(self, clusters):
+        cached_key = 'k8s.' + ','.join([cluster['id'] for cluster in sorted(clusters, key=lambda x: x['id'])
+                                        ]) + '.' + self.__class__.__name__
+        cache.delete(cached_key)
+
     def cached_all(self, clusters, expires=3):
         cached_key = 'k8s.' + ','.join([cluster['id'] for cluster in sorted(clusters, key=lambda x: x['id'])
                                         ]) + '.' + self.__class__.__name__
@@ -48,156 +55,201 @@ class BaseEntity:
 
 
 class Cluster(BaseEntity):
+    @classmethod
+    def to_dict(cls, cluster, item):
+        result = {
+            'id': item['id'],
+            'name': item['name'],
+            'displayName': item['name'],
+            'correlation_id': item['correlation_id']
+        }
+        return result
+
     def all(self, clusters):
         results = []
         for cluster in clusters:
-            results.append({
-                'id': cluster['id'],
-                'name': cluster['name'],
-                'displayName': cluster['name'],
-                'correlation_id': cluster['correlation_id']
-            })
+            results.append(self.to_dict(cluster, cluster))
         return results
 
 
 class Node(BaseEntity):
+    @classmethod
+    def to_dict(cls, cluster, item):
+        ip_address = None
+        for address in item.status.addresses:
+            if address.type == 'InternalIP':
+                ip_address = address.address
+                break
+        correlation_id = None
+        if item.metadata.labels:
+            for tag_key, tag_value in item.metadata.labels.items():
+                if tag_key == const.Tag.NODE_ID_TAG:
+                    correlation_id = tag_value
+                    break
+        result = {
+            'id': item.metadata.uid,
+            'name': item.metadata.name,
+            'displayName': f'{cluster["name"]}-{item.metadata.name}',
+            'ip_address': ip_address,
+            'cluster_id': cluster["id"],
+            'correlation_id': correlation_id,
+        }
+        return result
+
     def all(self, clusters):
         results = []
         for cluster in clusters:
             k8s_client = self.cluster_client(cluster)
             for item in k8s_client.list_node().items:
-                ip_address = None
-                for address in item.status.addresses:
-                    if address.type == 'InternalIP':
-                        ip_address = address.address
-                        break
-                correlation_id = None
-                if item.metadata.labels:
-                    for tag_key, tag_value in item.metadata.labels.items():
-                        if tag_key == const.Tag.NODE_ID_TAG:
-                            correlation_id = tag_value
-                            break
-                results.append({
-                    'id': item.metadata.uid,
-                    'name': item.metadata.name,
-                    'displayName': f'{cluster["name"]}-{item.metadata.name}',
-                    'ip_address': ip_address,
-                    'cluster_id': cluster["id"],
-                    'correlation_id': correlation_id,
-                })
+                results.append(self.to_dict(cluster, item))
         return results
 
 
 class Deployment(BaseEntity):
+    @classmethod
+    def to_dict(cls, cluster, item):
+        correlation_id = None
+        if item.metadata.labels:
+            for tag_key, tag_value in item.metadata.labels.items():
+                if tag_key == const.Tag.DEPLOYMENT_ID_TAG:
+                    correlation_id = tag_value
+                    break
+        result = {
+            'id': item.metadata.uid,
+            'name': item.metadata.name,
+            'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
+            'namespace': item.metadata.namespace,
+            'cluster_id': cluster["id"],
+            'correlation_id': correlation_id,
+        }
+        return result
+
     def all(self, clusters):
         results = []
         for cluster in clusters:
             k8s_client = self.cluster_client(cluster)
             for item in k8s_client.list_all_deployment().items:
-                correlation_id = None
-                if item.metadata.labels:
-                    for tag_key, tag_value in item.metadata.labels.items():
-                        if tag_key == const.Tag.DEPLOYMENT_ID_TAG:
-                            correlation_id = tag_value
-                            break
-                results.append({
-                    'id': item.metadata.uid,
-                    'name': item.metadata.name,
-                    'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
-                    'namespace': item.metadata.namespace,
-                    'cluster_id': cluster["id"],
-                    'correlation_id': correlation_id,
-                })
+                results.append(self.to_dict(cluster, item))
         return results
 
 
 class ReplicaSet(BaseEntity):
+    @classmethod
+    def to_dict(cls, cluster, item):
+        controll_by = None
+        if item.metadata.owner_references:
+            for owner in item.metadata.owner_references:
+                if owner.controller and owner.kind == 'Deployment':
+                    controll_by = owner.uid
+                    break
+        result = {
+            'id': item.metadata.uid,
+            'name': item.metadata.name,
+            'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
+            'namespace': item.metadata.namespace,
+            'deployment_id': controll_by,
+            'cluster_id': cluster["id"]
+        }
+        return result
+
     def all(self, clusters):
         results = []
         for cluster in clusters:
             k8s_client = self.cluster_client(cluster)
             for item in k8s_client.list_all_replica_set().items:
-                controll_by = None
-                if item.metadata.owner_references:
-                    for owner in item.metadata.owner_references:
-                        if owner.controller and owner.kind == 'Deployment':
-                            controll_by = owner.uid
-                            break
-                results.append({
-                    'id': item.metadata.uid,
-                    'name': item.metadata.name,
-                    'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
-                    'namespace': item.metadata.namespace,
-                    'deployment_id': controll_by,
-                    'cluster_id': cluster["id"]
-                })
+                results.append(self.to_dict(cluster, item))
         return results
 
 
 class Service(BaseEntity):
+    @classmethod
+    def to_dict(cls, cluster, item):
+        correlation_id = None
+        if item.metadata.labels:
+            for tag_key, tag_value in item.metadata.labels.items():
+                if tag_key == const.Tag.SERVICE_ID_TAG:
+                    correlation_id = tag_value
+                    break
+        result = {
+            'id': item.metadata.uid,
+            'name': item.metadata.name,
+            'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
+            'namespace': item.metadata.namespace,
+            'cluster_id': cluster["id"],
+            'ip_address': item.spec.cluster_ip,
+            'correlation_id': correlation_id,
+        }
+        return result
+
     def all(self, clusters):
         results = []
         for cluster in clusters:
             k8s_client = self.cluster_client(cluster)
             for item in k8s_client.list_all_service().items:
-                correlation_id = None
-                if item.metadata.labels:
-                    for tag_key, tag_value in item.metadata.labels.items():
-                        if tag_key == const.Tag.SERVICE_ID_TAG:
-                            correlation_id = tag_value
-                            break
-                results.append({
-                    'id': item.metadata.uid,
-                    'name': item.metadata.name,
-                    'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
-                    'namespace': item.metadata.namespace,
-                    'cluster_id': cluster["id"],
-                    'correlation_id': correlation_id,
-                })
+                results.append(self.to_dict(cluster, item))
         return results
 
 
 class Pod(BaseEntity):
+    @classmethod
+    def to_dict(cls, cluster, item):
+        correlation_id = None
+        if item.metadata.labels:
+            for tag_key, tag_value in item.metadata.labels.items():
+                if tag_key == const.Tag.POD_ID_TAG:
+                    correlation_id = tag_value
+                    break
+        controll_by = None
+        if item.metadata.owner_references:
+            for owner in item.metadata.owner_references:
+                if owner.controller and owner.kind == 'ReplicaSet':
+                    controll_by = owner.uid
+                    break
+        result = {
+            'id': item.metadata.uid,
+            'name': item.metadata.name,
+            'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
+            'namespace': item.metadata.namespace,
+            'ip_address': item.status.pod_ip,
+            'replicaset_id': controll_by,
+            'deployment_id': None,
+            'correlation_id': correlation_id,
+            'node_id': item.spec.node_name,
+            'cluster_id': cluster["id"],
+        }
+        # patch node_id
+        node_mapping = {}
+        nodes = Node().cached_all([cluster])
+        for node in nodes:
+            node_mapping.setdefault(node['cluster_id'], {}).setdefault(node['name'], node['id'])
+        # patch deployment_id
+        rs_mapping = {}
+        rss = ReplicaSet().cached_all([cluster])
+        for rs in rss:
+            rs_mapping.setdefault(rs['cluster_id'], {}).setdefault(rs['id'], rs['deployment_id'])
+        result['node_id'] = node_mapping.get(result['cluster_id'], {}).get(result['node_id'], None)
+        result['deployment_id'] = rs_mapping.get(result['cluster_id'], {}).get(result['replicaset_id'], None)
+        return result
+
     def all(self, clusters):
         results = []
         for cluster in clusters:
             k8s_client = self.cluster_client(cluster)
             for item in k8s_client.list_all_pod().items:
-                correlation_id = None
-                if item.metadata.labels:
-                    for tag_key, tag_value in item.metadata.labels.items():
-                        if tag_key == const.Tag.POD_ID_TAG:
-                            correlation_id = tag_value
-                            break
-                controll_by = None
-                if item.metadata.owner_references:
-                    for owner in item.metadata.owner_references:
-                        if owner.controller and owner.kind == 'ReplicaSet':
-                            controll_by = owner.uid
-                            break
-                results.append({
-                    'id': item.metadata.uid,
-                    'name': item.metadata.name,
-                    'displayName': f'{cluster["name"]}-{item.metadata.namespace}-{item.metadata.name}',
-                    'namespace': item.metadata.namespace,
-                    'deployment_id': controll_by,
-                    'correlation_id': correlation_id,
-                    'node_id': item.spec.node_name,
-                    'cluster_id': cluster["id"],
-                })
-        # patch node_id
-        node_mapping = {}
-        if results:
-            nodes = Node().cached_all(clusters)
-            for node in nodes:
-                node_mapping.setdefault(node['cluster_id'], {}).setdefault(node['name'], node['id'])
-        # patch deployment_id
-        rs_mapping = {}
-        if results:
-            rss = ReplicaSet().cached_all(clusters)
-            for rs in rss:
-                rs_mapping.setdefault(rs['cluster_id'], {}).setdefault(rs['id'], rs['deployment_id'])
-        for pod in results:
-            pod['node_id'] = node_mapping.get(pod['cluster_id'], {}).get(pod['node_id'], None)
-            pod['deployment_id'] = rs_mapping.get(pod['cluster_id'], {}).get(pod['deployment_id'], None)
+                results.append(self.to_dict(cluster, item))
         return results
+
+    def watch(self, cluster, event_stop, notify):
+        k8s_client = self.cluster_client(cluster)
+        current_time = datetime.datetime.now(datetime.timezone.utc)
+        w = watch.Watch()
+        for event in w.stream(k8s_client.core_client.list_pod_for_all_namespaces):
+            if event['type'] == 'ADDED':
+                # new -> alert
+                if event['object'].metadata.creation_timestamp >= current_time:
+                    notify('POD.ADDED', cluster['id'], self.to_dict(cluster, event['object']))
+            elif event['type'] == 'DELETED':
+                # delete -> alert
+                notify('POD.DELETED', cluster['id'], self.to_dict(cluster, event['object']))
+            if event_stop.is_set():
+                w.stop()
