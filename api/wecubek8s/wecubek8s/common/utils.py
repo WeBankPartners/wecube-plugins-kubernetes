5d1b5a63358ce9646fd9e063f5ea7727903b5488
roywu <roywu@webank.com>
2021-04-01 16:33:54 +0800

    encrypt cluster token

--- api/wecubek8s/wecubek8s/common/utils.py
+++ api/wecubek8s/wecubek8s/common/utils.py
@@ -238,90 +238,6 @@ class ClientMixin:
         return resp_json
 
 
-# def transform_filter_to_cmdb_query(filters, orders=None, offset=None, limit=None, fields_mapping=None):
-#     '''transform filter from talos filters to wecmdb filters
-#     '''
-#     def _reverse_mapping(mappings):
-#         '''reverse origin_field=>field mapping to field=>origin_field
-#         '''
-#         new_mappings = {}
-#         for map_key, map_value in mappings.items():
-#             new_mappings[map_value] = map_key
-#         return new_mappings
-
-#     def _transform_operator(operator):
-#         '''transform talos operator to wecmdb operator
-#         '''
-#         mappings = {'ilike': 'contains', 'like': 'contains'}
-#         return mappings.get(operator) or operator
-
-#     def _transform_filter_field(filters, mappings):
-#         '''transform talos filters to wecmdb filters
-#         '''
-#         new_filters = {}
-#         for filter_key, filter_value in filters.items():
-#             if filter_key in mappings:
-#                 new_filters[mappings[filter_key]] = filter_value
-#             else:
-#                 new_filters[filter_key] = filter_value
-#         return new_filters
-
-#     def _transform_order_field(orders, mappings):
-#         new_orders = []
-#         for order in orders:
-#             if order.startswith('+'):
-#                 if order[1:] in mappings:
-#                     new_orders.append('+' + mappings[order[1:]])
-#                 else:
-#                     new_orders.append(order)
-#             elif order.startswith('-'):
-#                 if order[1:] in mappings:
-#                     new_orders.append('-' + mappings[order[1:]])
-#                 else:
-#                     new_orders.append(order)
-#             else:
-#                 if order in mappings:
-#                     new_orders.append(mappings[order])
-#                 else:
-#                     new_orders.append(order)
-#         return new_orders
-
-#     if fields_mapping:
-#         fields_mapping = _reverse_mapping(fields_mapping)
-#     if filters and fields_mapping:
-#         filters = _transform_filter_field(filters, fields_mapping)
-#     if orders and fields_mapping:
-#         orders = _transform_order_field(orders, fields_mapping)
-#     query = {"dialect": {"showCiHistory": False}, "filters": [], "paging": False}
-#     if offset and limit:
-#         query['paging'] = True
-#         query['pageable'] = {"pageSize": limit, "startIndex": offset}
-#     if orders:
-#         sortings = []
-#         for order in orders:
-#             if order.startswith('+'):
-#                 sortings.append({"asc": True, "field": order[1:]})
-#             elif order.startswith('-'):
-#                 sortings.append({"asc": False, "field": order[1:]})
-#             else:
-#                 sortings.append({"asc": True, "field": order})
-#         query['sortings'] = sortings
-#     if filters:
-#         new_filters = []
-#         for filter_key, filter_value in filters.items():
-#             if isinstance(filter_value, dict):
-#                 for value_key, value_value in filter_value.items():
-#                     new_filters.append({
-#                         "name": filter_key,
-#                         "operator": _transform_operator(value_key),
-#                         "value": value_value
-#                     })
-#             else:
-#                 new_filters.append({"name": filter_key, "operator": "eq", "value": filter_value})
-#         query['filters'] = new_filters
-#     return query
-
-
 def transform_filter_to_entity_query(filters, fields_mapping=None):
     '''transform filter from talos filters to entity filters
     '''
@@ -377,8 +293,32 @@ def md5(text):
     return hasher.hexdigest()
 
 
-def aes_cbc_pkcs7_decrypt(encrypted, key, iv):
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    data = cipher.decrypt(encrypted)
-    # pkcs7 padding
-    return data[0:-data[-1]]
+def platform_encrypt(text, guid, seed):
+    result = text
+    encrypted_prefix = '{cipher_a}'
+    if not text.startswith(encrypted_prefix):
+        key = md5(guid + seed)[:16]
+        cipher = AES.new(key, AES.MODE_CBC, key)
+        text = utils.ensure_bytes(text)
+        # pkcs7 padding
+        amount_to_pad = AES.block_size - (len(text) % AES.block_size)
+        if amount_to_pad == 0:
+            amount_to_pad = AES.block_size
+        pad = chr(amount_to_pad).encode()
+        encrypted_text = cipher.encrypt(text + pad * amount_to_pad)
+        result = '{cipher_a}' + encrypted_text.hex()
+    return result
+
+
+def platform_decrypt(text, guid, seed):
+    result = text
+    encrypted_prefix = '{cipher_a}'
+    if text.startswith(encrypted_prefix):
+        encrypted_text = text[len(encrypted_prefix):]
+        encrypted_text = bytes.fromhex(encrypted_text)
+        key = md5(guid + seed)[:16]
+        cipher = AES.new(key, AES.MODE_CBC, key)
+        origin_text = cipher.decrypt(encrypted_text)
+        origin_text = origin_text[0:-origin_text[-1]]
+        result = origin_text.decode()
+    return result
