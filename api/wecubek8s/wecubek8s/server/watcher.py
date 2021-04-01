60f17d75a6e0830679a882e3fda5a432ce0b00e7
roywu <roywu@webank.com>
2021-04-01 11:06:36 +0800

    add pod watcher

---
+++ api/wecubek8s/wecubek8s/server/watcher.py
@@ -0,0 +1,88 @@
+# coding=utf-8
+from __future__ import absolute_import
+import logging
+import time
+from threading import Event
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
+from wecubek8s.server.wsgi_server import application
+from wecubek8s.apps.model import api
+
+LOG = logging.getLogger(__name__)
+
+
+def notify_pod(event, cluster_id, data):
+    # TODO: handle added,deleted
+    print(event, cluster_id, data)
+
+
+def watch_pod(cluster, event_stop):
+    while not event_stop.is_set():
+        try:
+            api.Pod().watch(cluster, event_stop, notify_pod)
+        except Exception as e:
+            LOG.error('exception raised while watching pod from %s', cluster['id'])
+            LOG.exception(e)
+            time.sleep(0.5)
+
+
+def cluster_equal(cluster1, cluster2):
+    cluster1 = cluster1.copy()
+    cluster2 = cluster2.copy()
+    fields = ['name', 'displayName', 'correlation_id', 'created_by', 'created_time', 'updated_by', 'updated_time']
+    for f in fields:
+        cluster1.pop(f, None)
+    for f in fields:
+        cluster2.pop(f, None)
+    return cluster1 == cluster2
+
+
+def main():
+    LOG.info('starting watcher')
+    pool = PoolExecutor(100)
+    cluster_maping = {}
+    while True:
+        latest_clusters = api.db_resource.Cluster().list()
+        latest_cluster_maping = dict(
+            zip([cluster['id'] for cluster in latest_clusters], [cluster for cluster in latest_clusters]))
+        watching_cluster_ids = set(list(cluster_maping.keys()))
+        latest_cluster_ids = set(list(latest_cluster_maping.keys()))
+        new_cluster_ids = latest_cluster_ids - watching_cluster_ids
+        del_cluster_ids = watching_cluster_ids - latest_cluster_ids
+        mod_cluster_ids = latest_cluster_ids & watching_cluster_ids
+
+        if new_cluster_ids:
+            for cluster_id in new_cluster_ids:
+                LOG.info('start watching pod from newly cluster: %s', cluster_id)
+                cluster = latest_cluster_maping[cluster_id]
+                event_stop = Event()
+                pool.submit(watch_pod, cluster, event_stop)
+                # add mapping
+                cluster_maping[cluster_id] = (cluster, event_stop)
+        if del_cluster_ids:
+            for cluster_id in del_cluster_ids:
+                LOG.info('stop watching pod from deleted cluster: %s', cluster_id)
+                cluster, event_stop = cluster_maping[cluster_id]
+                event_stop.set()
+                del cluster_maping[cluster_id]
+
+        if mod_cluster_ids:
+            for cluster_id in mod_cluster_ids:
+                cluster, event_stop = cluster_maping[cluster_id]
+                latest_cluster = latest_cluster_maping[cluster_id]
+                if not cluster_equal(latest_cluster, cluster):
+                    LOG.info('stop watching pod from modified cluster: %s', cluster_id)
+                    cluster, event_stop = cluster_maping[cluster_id]
+                    event_stop.set()
+                    del cluster_maping[cluster_id]
+                    LOG.info('start watching pod from modified cluster: %s', cluster_id)
+                    event_stop = Event()
+                    pool.submit(watch_pod, latest_cluster, event_stop)
+                    # add mapping
+                    cluster_maping[cluster_id] = (latest_cluster, event_stop)
+        time.sleep(1)
+
+    pool.shutdown(True)
+
+
+if __name__ == '__main__':
+    main()
--- build/start_all.sh
+++ build/start_all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # log rotate
 nohup wecubek8s_scheduler > /dev/null 2>&1 &
+nohup wecubek8s_watcher > /dev/null 2>&1 &
 # wsgi api server
 /usr/local/bin/gunicorn --config /etc/wecubek8s/gunicorn.py wecubek8s.server.wsgi_server:application
