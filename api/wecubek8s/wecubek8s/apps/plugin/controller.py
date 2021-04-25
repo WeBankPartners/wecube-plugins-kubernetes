5d35e86d91f07713f897cf952d03b5ac3b15c3b7
roywu <roywu@webank.com>
2021-04-25 09:30:46 +0800

    rename remove to destroy

--- api/wecubek8s/wecubek8s/apps/plugin/controller.py
+++ api/wecubek8s/wecubek8s/apps/plugin/controller.py
@@ -26,8 +26,8 @@ class Deployment(controller.Plugin):
         self.set_item_default(clean_item)
         return clean_item
 
-    def validate_item_remove(self, item_index, item):
-        clean_item = crud.ColumnValidator.get_clean_data(rules.remove_rules, item, 'check')
+    def validate_item_destroy(self, item_index, item):
+        clean_item = crud.ColumnValidator.get_clean_data(rules.destroy_rules, item, 'check')
         if not clean_item.get('namespace'):
             clean_item['namespace'] = 'default'
         return clean_item
@@ -35,7 +35,7 @@ class Deployment(controller.Plugin):
     def apply(self, reqid, operator, item_index, item, **kwargs):
         return plugin_api.Deployment().apply(item)
 
-    def remove(self, reqid, operator, item_index, item, **kwargs):
+    def destroy(self, reqid, operator, item_index, item, **kwargs):
         return plugin_api.Deployment().remove(item)
 
 
@@ -64,8 +64,8 @@ class Service(controller.Plugin):
             clean_item['instances'][idx] = clean_instance
         return clean_item
 
-    def validate_item_remove(self, item_index, item):
-        clean_item = crud.ColumnValidator.get_clean_data(rules.remove_rules, item, 'check')
+    def validate_item_destroy(self, item_index, item):
+        clean_item = crud.ColumnValidator.get_clean_data(rules.destroy_rules, item, 'check')
         if not clean_item.get('namespace'):
             clean_item['namespace'] = 'default'
         return clean_item
@@ -73,5 +73,5 @@ class Service(controller.Plugin):
     def apply(self, reqid, operator, item_index, item, **kwargs):
         return plugin_api.Service().apply(item)
 
-    def remove(self, reqid, operator, item_index, item, **kwargs):
+    def destroy(self, reqid, operator, item_index, item, **kwargs):
         return plugin_api.Service().remove(item)
