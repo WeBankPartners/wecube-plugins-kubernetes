31fa742f4c0b4ea10df1145df41cef04c680dd21
Alexaa123 <1239327906@qq.com>
2025-11-17 10:48:16 +0800

    解决sql的问题

--- init.sql
+++ init.sql
@@ -5,9 +5,8 @@ CREATE TABLE `cluster` (
   `correlation_id` varchar(36) DEFAULT NULL,
   `api_server` varchar(255) NOT NULL,
   `token` varchar(2048) NOT NULL,
-  `image_pull_username` varchar(255) DEFAULT NULL,
-  `image_pull_password` varchar(255) DEFAULT NULL,
-  `private_registry` varchar(255) DEFAULT NULL,
+  `metric_host` varchar(63) DEFAULT NULL,
+  `metric_port` varchar(20) DEFAULT NULL,
   `created_by` varchar(36) DEFAULT NULL,
   `created_time` datetime DEFAULT NULL,
   `updated_by` varchar(36) DEFAULT NULL,
@@ -17,3 +16,15 @@ CREATE TABLE `cluster` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+#@v0.1.4-begin@;
+-- 删除旧字段
+ALTER TABLE `cluster` DROP COLUMN IF EXISTS `metric_host`;
+ALTER TABLE `cluster` DROP COLUMN IF EXISTS `metric_port`;
+
+-- 添加新字段
+ALTER TABLE `cluster` ADD COLUMN `image_pull_username` varchar(255) DEFAULT NULL COMMENT '镜像仓库用户名';
+ALTER TABLE `cluster` ADD COLUMN `image_pull_password` varchar(255) DEFAULT NULL COMMENT '镜像仓库密码';
+ALTER TABLE `cluster` ADD COLUMN `private_registry` varchar(255) DEFAULT NULL COMMENT '私有镜像仓库地址';
+
+#@v0.1.4-end@;
